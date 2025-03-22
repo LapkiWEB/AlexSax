8255be49093f288f9270e1ef2939b6804a240879
Stepan <lapki.web@gmail.com>
2025-03-22 19:32:35 +0800

    add header + main-screen + offer + adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,24 @@
 // WOW JS Конфликтует с оверфлоу хайден на бади или секций
+$(function () {
+
+  $('.menu__btn').on('click', (function () {
+    if (!$('.nav').hasClass('active')) {
+      $('.nav').addClass('active');
+      $('.menu__btn').addClass('active');
+      $('body').addClass('menu-opened');
+      $('.menu-drop').on('click', (e) => {
+        e.target.classList.toggle('active');
+      })
+    } else {
+      $('.nav').removeClass('active');
+      $('.menu__btn').removeClass('active');
+      $('body').removeClass('menu-opened');
+      $('.menu-drop').on('click', (e) => {
+        e.target.classList.toggle('active');
+      })
+    }
+
+
+
+  }));
+});
